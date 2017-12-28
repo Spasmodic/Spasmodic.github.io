38c5ee42e79e6e46fd8e96ef61cac9f054920d04
Spas Modic <spasmac@Spass-Mac.local>
2017-12-27 17:40:05 -0800

    ReSet

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { SPage } from './app.po';
 
-describe('spas-portfo App', () => {
-  let page: AppPage;
+describe('s App', function() {
+  let page: SPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new SPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
